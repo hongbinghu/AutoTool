355fce2b5b2976b75f8cdd00fae6a05ea49b6ce4
HongBingHu <2846422519@qq.com>
2022-04-24 16:04:20 +0800

    集客标准地址导入改进版

--- address_up.py
+++ address_up.py
@@ -6,31 +6,31 @@ import pymysql
 import sys
 
 # open为内置函数，w参数是为了打开文件的写权限
-a = open("./insert_address.sql", "w")
-sys.stdout = a
-
-conn3 = pymysql.connect(
-    host='10.30.107.152',
-    port=3810,
-    database='nc_resource',
-    user='nc_resource_user',
-    password='Li_9n8xTa6',
-    charset='utf8',
-    autocommit=True
-)
+# a = open("./insert_address.sql", "w")
+# sys.stdout = a
 
 # conn3 = pymysql.connect(
-#     host='localhost',
-#     port=3306,
+#     host='10.30.107.152',
+#     port=3810,
 #     database='nc_resource',
-#     user='root',
-#     password='root',
+#     user='nc_resource_user',
+#     password='Li_9n8xTa6',
 #     charset='utf8',
 #     autocommit=True
 # )
 
+conn3 = pymysql.connect(
+    host='localhost',
+    port=3306,
+    database='nc_resource',
+    user='root',
+    password='root',
+    charset='utf8',
+    autocommit=True
+)
+
 # 读取Excel中B列A端标准地址信息
-df = pandas.read_excel('./标准地址导入模板.xlsx', usecols=[1], names=None)
+df = pandas.read_excel('C:\\Users\\HongBingHu\\Desktop\\标准地址导入模板.xlsx', usecols=[1], names=None)
 # 读取数据中为空的nan替换成None
 # df[pandas.isna(df)] = None
 df.fillna('None', inplace=True)
@@ -42,7 +42,7 @@ for i in A_list:
     A_result.append(i[0])
 
 # 查询标准地址是否存在
-sql_1 = '''SELECT NAME,UUID FROM `nc_resource`.`address` WHERE NAME in %s'''
+sql_1 = '''SELECT NAME,UUID FROM `ne_resource`.`address` WHERE NAME in %s'''
 cursor = conn3.cursor()
 cursor.execute(sql_1, [A_result])
 # 返回查询结果
@@ -53,12 +53,19 @@ chaxun = []
 for i in a_res:
     chaxun.append(list(i))
 
+for i in chaxun:
+    print(i)
+
 # 读取A列B列数据存入列表AB = []
-df1 = pandas.read_excel('./标准地址导入模板.xlsx', usecols=[0, 1], names=None)
+df1 = pandas.read_excel('C:\\Users\\HongBingHu\\Desktop\\标准地址导入模板.xlsx', usecols=[0, 1], names=None)
 # 替换Excel中的空值
 df1.fillna('None', inplace=True)
 # 返回list结构值,将AB列数据存入列表中
 AB_list = df1.values.tolist()
+
+print('-------ABlist-------')
+for i in AB_list:
+    print(i)
 
 # A端标准地址在存量中能查询到的数据 19条
 new_AB = []
@@ -76,10 +83,12 @@ for i in new_AB:
 for i in new_AB:
     sql_2 = "UPDATE design d SET d.standard_address='%s',d.address_uuid='%s' WHERE d.uuid = (SELECT real_connectivity_uuid FROM product p,connectivity c WHERE p.uuid = c.service_uuid AND c.service_type=1 AND p.product_code='%s' AND p.direction=1);" % (i[1], i[2], i[0])
     print(sql_2)
+print('-- 123--------------------------------------------------------------------------------------')
+
 
 print('-- --------------------------------------------------------------------------------------')
 # 读取Excel中C列Z端标准地址信息
-df2 = pandas.read_excel('./标准地址导入模板.xlsx', usecols=[2], names=None)
+df2 = pandas.read_excel('C:\\Users\\HongBingHu\\Desktop\\标准地址导入模板.xlsx', usecols=[2], names=None)
 # 读取数据中为空的nan替换成None
 # df[pandas.isna(df)] = None
 df2.fillna('None', inplace=True)
@@ -90,7 +99,7 @@ Z_result = []
 for i in Z_list:
     Z_result.append(i[0])
 # 查询标准地址是否存在
-sql_3 = '''SELECT NAME,UUID FROM `nc_resource`.`address` WHERE NAME in %s'''
+sql_3 = '''SELECT NAME,UUID FROM `ne_resource`.`address` WHERE NAME in %s'''
 cursor = conn3.cursor()
 cursor.execute(sql_3, [Z_result])
 # 返回查询结果
@@ -102,7 +111,7 @@ for i in Z_res:
     Z_chaxun.append(list(i))
 
 # 读取A列C列数据存入列表AB = []
-df3 = pandas.read_excel('./标准地址导入模板.xlsx', usecols=[0, 2], names=None)
+df3 = pandas.read_excel('C:\\Users\\HongBingHu\\Desktop\\标准地址导入模板.xlsx', usecols=[0, 2], names=None)
 # 替换Excel中的空值
 df3.fillna('None', inplace=True)
 # 返回list结构值,将AB列数据存入列表中
@@ -140,6 +149,9 @@ for i in chaxun:
         if i[0] == j[1]:
             cp_AB.remove(j)
             sc()
+print('----i-')
+for i in cp_AB:
+    print(i)
 
 # Z端标准地址在存量中查询不到的数据
 cp_AC = AB_list2
@@ -157,24 +169,30 @@ for i in Z_chaxun:
 
 
 # 开始生成excel,基础数据,A端标准地址已更新数据,Z端标准地址已更新数据,A端标准地址未更新数据Z端标准地址未更新数据
-ps1 = pandas.read_excel('./标准地址导入模板.xlsx', usecols='A:C', names=None)
+ps1 = pandas.read_excel('C:\\Users\\HongBingHu\\Desktop\\标准地址导入模板.xlsx', usecols='A:C', names=None)
 # 替换Excel中的空值
 ps1.fillna('', inplace=True)
 # 返回list结构值,将AB列数据存入列表中
 ps_list = ps1.values.tolist()
 
 
-lujin = pandas.ExcelWriter('./标准地址更新数据导出.xlsx')
+lujin = pandas.ExcelWriter('C:\\Users\\HongBingHu\\Desktop\\标准地址更新数据导出.xlsx')
 
-form_header = ['产品实例标识', 'A端标准地址', 'Z端标准地址']
-form_header1 = ['产品实例标识', 'A端标准地址', 'A端标准地址uuid']
-form_header2 = ['产品实例标识', 'Z端标准地址', 'Z端标准地址uuid']
-form_header3 = ['产品实例标识', 'A端标准地址存量不存在']
-form_header4 = ['产品实例标识', 'Z端标准地址存量不存在']
-pandas.DataFrame(ps_list, columns=form_header).to_excel(lujin, sheet_name='基础数据', index=None)
-pandas.DataFrame(new_AB, columns=form_header1).to_excel(lujin, sheet_name='A端标准地址已更新数据', index=None)
-pandas.DataFrame(new_AC, columns=form_header2).to_excel(lujin, sheet_name='Z端标准地址已更新数据', index=None)
-pandas.DataFrame(cp_AB, columns=form_header3).to_excel(lujin, sheet_name='A端标准地址不存数据', index=None)
-pandas.DataFrame(cp_AC, columns=form_header4).to_excel(lujin, sheet_name='Z端标准地址不存在数据', index=None)
+# form_header = ['产品实例标识', 'A端标准地址', 'Z端标准地址']
+# form_header1 = ['产品实例标识', 'A端标准地址', 'A端标准地址uuid']
+# form_header2 = ['产品实例标识', 'Z端标准地址', 'Z端标准地址uuid']
+# form_header3 = ['产品实例标识', 'A端标准地址存量不存在']
+# form_header4 = ['产品实例标识', 'Z端标准地址存量不存在']
+# pandas.DataFrame(ps_list, columns=form_header).to_excel(lujin, sheet_name='基础数据', index=None)
+# pandas.DataFrame(new_AB, columns=form_header1).to_excel(lujin, sheet_name='A端标准地址已更新数据', index=None)
+# pandas.DataFrame(new_AC, columns=form_header2).to_excel(lujin, sheet_name='Z端标准地址已更新数据', index=None)
+# pandas.DataFrame(cp_AB, columns=form_header3).to_excel(lujin, sheet_name='A端标准地址不存数据', index=None)
+# pandas.DataFrame(cp_AC, columns=form_header4).to_excel(lujin, sheet_name='Z端标准地址不存在数据', index=None)
+
+pandas.DataFrame(ps_list).to_excel(lujin, sheet_name='基础数据', index=None)
+pandas.DataFrame(new_AB).to_excel(lujin, sheet_name='A端标准地址已更新数据', index=None)
+pandas.DataFrame(new_AC).to_excel(lujin, sheet_name='Z端标准地址已更新数据', index=None)
+pandas.DataFrame(cp_AB).to_excel(lujin, sheet_name='A端标准地址不存数据', index=None)
+pandas.DataFrame(cp_AC).to_excel(lujin, sheet_name='Z端标准地址不存在数据', index=None)
 
 lujin.save()
