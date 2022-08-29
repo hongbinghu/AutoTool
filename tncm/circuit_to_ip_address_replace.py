ee638c23a461738522ef345f6f575cb0b216d406
HongBingHu <2846422519@qq.com>
2022-08-29 17:51:33 +0800

    调度库:circuit_to_ip_address同步资源库circuit_to_ip_address

--- tncm/circuit_to_ip_address_replace.py
+++ tncm/circuit_to_ip_address_replace.py
@@ -13,10 +13,12 @@ sys.stdout = a
 
 # 配置调度库连接信息
 conn = pymysql.connect(host='10.30.107.156', port=3810, user='rc_tncm_user', password='W+R56Lr6Ky', database='rc_tncm', charset='utf8', autocommit=True)
+conn1 = pymysql.connect(host='10.30.107.152', port=3810, database='nc_resource', user='nc_resource_user', password='Li_9n8xTa6', charset='utf8', autocommit=True)
 # conn = pymysql.connect(host='localhost', port=3306, user='root', password='root', database='rc_tncm', charset='utf8', autocommit=True)
+# conn1 = pymysql.connect(host='localhost', port=3306, user='root', password='root', database='nc_resource', charset='utf8', autocommit=True)
 
 # 1.读取Excel数据到集合中
-df = pandas.read_excel('C:\\Users\\HongBingHu\\Desktop\\IP地址替换8-29.xlsx', names=None)
+df = pandas.read_excel('./电路ip地址替换模板.xlsx', names=None)
 # 2.读取数据存入二维列表中
 A_list = df.values.tolist()
 
@@ -24,6 +26,8 @@ cursor = conn.cursor()
 sql_1 = '''SELECT cp.uuid FROM circuit_to_ip_address cp,circuit ct 
 	WHERE cp.circuit_uuid = ct.uuid AND ct.name = %s AND ip_address_name = %s limit 1
 '''
+cursor1 = conn1.cursor()
+sql_0 = "SELECT uuid FROM `nc_resource`.`ipv4_addr` WHERE NAME = %s"
 # 定义一个列表
 new_cir_ip_exists = []
 new_cir_ip_no = []
@@ -32,19 +36,22 @@ c = 0
 for i in A_list:
     total = cursor.execute(sql_1,[i[0],i[1]])
     uuid = cursor.fetchall()
-    #print(uuid[0][0])
+    cursor1.execute(sql_0, i[2])
+    ip_uuid = cursor1.fetchall();
     if total != 0:
         new_cir_ip_exists.append(i)
         new_cir_ip_exists[c].append(uuid[0][0])
+        new_cir_ip_exists[c].append(ip_uuid[0][0])
+        c = c + 1
     else:
         # 存入为查询到uuid的信息
         new_cir_ip_no.append(i)
-    c = c + 1
+
 
 # 遍历uuid存在的集合
 print("###################################调度库与资源库更新电路ip地址信息############################################")
 for i in new_cir_ip_exists:
-    sql_2 = "UPDATE circuit_to_ip_address SET ip_address_name = '%s' WHERE UUID = '%s';" % (i[2], i[3])
+    sql_2 = "UPDATE circuit_to_ip_address SET ip_address_name = '%s',ip_adress_uuid = '%s' WHERE UUID = '%s';" % (i[2], i[4], i[3])
     print(sql_2, file=a)
 print("###################################调度库与资源库更新电路ip地址信息############################################")
 print("###################################资源库更新新ip为占用地址信息############################################")
@@ -63,7 +70,7 @@ print("###################################资源库更新老ip为空闲地址信
 # 输出结果到excel中
 st = time.strftime("%Y%m%d%H%M%S", time.localtime())
 lujin = pandas.ExcelWriter('./电路ip地址更新信息导出' + st + '.xlsx')
-pandas.DataFrame(new_cir_ip_exists).to_excel(lujin, sheet_name='已更新电路IP信息', index=None)
-pandas.DataFrame(new_cir_ip_no).to_excel(lujin, sheet_name='ip+电路未查询数据信息', index=None)
+pandas.DataFrame(new_cir_ip_exists).to_excel(lujin, sheet_name='已更新电路IP信息', index=False, header=None)
+pandas.DataFrame(new_cir_ip_no).to_excel(lujin, sheet_name='ip+电路未查询数据信息', index=False, header=None)
 
 lujin.save()
